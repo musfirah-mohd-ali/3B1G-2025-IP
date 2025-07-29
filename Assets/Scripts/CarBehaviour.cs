6edc8cb2ab1db38fdfb2bce048df2219acd4fb92
Reito <s10265971@connect.np.edu.sg>
2025-07-29 15:41:58 +0800

    I have implemented front and back movement for car FINALLY

--- Assets/Scripts/CarBehaviour.cs
+++ Assets/Scripts/CarBehaviour.cs
@@ -4,14 +4,41 @@ public class CarBehaviour : MonoBehaviour
 {
     public Rigidbody SphereRB;
 
+    private float moveInput;
+
+    public float fwdSpeed = 10f;
+    public float revSpeed = 100f;
+
     void Start()
     {
-        
+        //detach rigidbody from car
+        SphereRB.transform.parent = null;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        moveInput = Input.GetAxisRaw("Vertical");
+        moveInput *= fwdSpeed;
+
+        if (moveInput > 0)
+        {
+            moveInput *= fwdSpeed;
+        }
+        else if (moveInput < 0)
+        {
+            moveInput *= revSpeed;
+        }
+
         transform.position = SphereRB.transform.position;
+
+
     }
+
+    private void FixedUpdate()
+    {
+        SphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+    }
+
 }
