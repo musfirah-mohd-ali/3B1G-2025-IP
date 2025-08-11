e96352421697e23a234a53d20f9fb6a94a6e1409
musfirah-mohd-ali <s10270156@connect.np.edu.sg>
2025-08-11 17:15:34 +0800

    Made a day to night transition

---
+++ Assets/Scripts/LightingManager.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+[ExecuteAlways]
+public class LightingManager : MonoBehaviour
+{
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField] private LightingPreset Preset;
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
+    [SerializeField] private float timeMultiplier = 0f;
+
+
+    private void Update()
+    {
+        if (Preset == null)
+        {
+            return;
+        }
+        if (Application.isPlaying)
+        {
+            TimeOfDay += Time.deltaTime * timeMultiplier;
+            TimeOfDay %= 24;
+            UpdateLighting(TimeOfDay / 24f);
+        }
+        else
+        {
+            UpdateLighting(TimeOfDay / 24f);
+        }
+    }
+    private void UpdateLighting(float timePercent)
+    {
+        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
+
+        if (DirectionalLight != null)
+        {
+            DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
+            DirectionalLight.transform.rotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0f));
+        }
+    }
+    private void OnValidate()
+    {
+        if (DirectionalLight != null)
+        {
+            return;
+        }
+        if (RenderSettings.sun != null)
+        {
+            DirectionalLight = RenderSettings.sun;
+        }
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach (Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    DirectionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+}
